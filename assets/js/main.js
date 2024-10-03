f91da1e0263acf9378739e489aba12624a466db5
Emiliano Rotta <emilianorottaf@gmail.com>
2024-10-02 21:39:27 -0300

    async await

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,19 +94,19 @@
 //------------------------------------------------------
 //Consigna: Implementa una función que simule un proceso de validación. La promesa debe resolver si la validación es exitosa o rechazar si falla.
 
-function validacionUsuario(usuario) { 
-    return new Promise((resolve, reject) => {
-        if(usuario === "Admin"){
-            resolve("Usuario Valido")
-        } else{
-            reject(`${usuario} no es valido`)
-        }
-    })
-}
+// function validacionUsuario(usuario) { 
+//     return new Promise((resolve, reject) => {
+//         if(usuario === "Admin"){
+//             resolve("Usuario Valido")
+//         } else{
+//             reject(`${usuario} no es valido`)
+//         }
+//     })
+// }
 
-validacionUsuario("usuario normal")
-.then(datos =>console.log(datos))
-.catch(error => console.log(error))
+// validacionUsuario("usuario normal")
+// .then(datos =>console.log(datos))
+// .catch(error => console.log(error))
 
 //----------------------------------------------
 
@@ -116,5 +116,77 @@ validacionUsuario("usuario normal")
 
 // Consigna: Simula una operación de lectura de datos que falla el 50% del tiempo. Utiliza promesas para manejar ambos casos.   //Math.random() > 0.5
 
+// function leerDatos() {
+//     return new Promise((resolve, reject) => {
+//         const exito = Math.random() > 0.5
+
+//         if (exito) {
+//             resolve("Datos leidos con exito")
+//         } else {
+//             reject("Error al leer datos")
+//         }
+
+//     })
+// }
+// leerDatos()
+//     .then(datos => console.log(datos))
+//     .catch(error => console.log(error))
+//
 // Consigna: Crea una función que devuelva una promesa que se resuelve con el resultado de una suma de dos números, después de 2 segundos.
 
+// function sumar(a, b){
+//     return new Promise((resolve) => {
+//         setTimeout(()=>{
+//             resolve(a+b)
+//         },2000)
+
+//     })
+// }
+
+// sumar(10,5).then(resultado =>console.log("Resultado " + resultado))
+
+
+
+
+//---------------------------------------------------------
+//                 Async Await
+//---------------------------------------------------------
+
+// async function ejemplo() {
+//     try {
+//         const respuesta = await fetch("https://rickandmortyapi.com/api/character")
+//         const data = await respuesta.json()
+//         let resultado = data.results
+//         for (let i = 0; i < resultado.length; i++) {
+//             console.log(resultado[i].name)
+//         }
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+
+// }
+
+// ejemplo()
+
+
+// async function obtenerDatos() {
+//     try {
+//         const respuesta = await fetch("https://jsonplaceholder.typicode.com/posts/")
+//         const data = await respuesta.json();
+//         const respuestaRAM =  await fetch("https://rickandmortyapi.com/api/character")
+//         const dataRAM = await respuestaRAM.json();
+//         console.log(`${dataRAM.results[0].name} lee el siguiente post: ${data[0].title}`)
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+// }
+// obtenerDatos()
+
+// Api de Harry Potter
+// https://hp-api.herokuapp.com/api/characters
+
+// Ejercicio 1: 
+// Escribe una función asíncrona que realice dos peticiones a la API de Harry Potter para obtener información sobre dos personajes  y combine los resultados. Maneja los errores con try/catch.
+
+//Ejercicio 2: 
+// Escribe una función asíncrona que realice una peticion a la API de Harry Potter. Muestra en consola la mayor cantidad de informacion de "Ron Weasley". Maneja los errores con try/catch.
